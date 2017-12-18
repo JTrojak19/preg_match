22c125cc75046f4d0ee6fb36f6e57400af55235f
Joanna Trojak <joannatrojak19@gmail.com>
2017-12-18 10:50:47 +0100

    pregMatchPrice

--- preg_match.php
+++ preg_match.php
@@ -12,16 +12,22 @@ $data = curl($subject);
 function pregMatchName(string $data){
     $pattern = "#big-text\">\s+[A-Z]+#";
     preg_match($pattern, $data, $matches);
-    //return $matches;
     $name = $matches[0];
     $patternToReplace = "#[^A-Z]+#";
     $replaced = preg_replace($patternToReplace, "", $name);
     return "Name: ".$replaced;
-
 }
 echo pregMatchName($data);
+function pregMatchPrice(string $data){
+    $pattern = "#price__number\">.*<+#";
+    preg_match($pattern, $data, $matches);
+    $price = $matches[0];
+    $patternToReplace = "#[^0-9]+#";
+    $replaced = preg_replace($patternToReplace, "", $price);
+    return "<br>Price: ".$replaced;
 
-
+}
+echo pregMatchPrice($data);
 
 
 
