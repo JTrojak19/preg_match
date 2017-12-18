e02e00f5a9d365a7a635f8a574efc1b274a3acc9
Joanna Trojak <joannatrojak19@gmail.com>
2017-12-18 10:29:39 +0100

    pregMatchName

--- preg_match.php
+++ preg_match.php
@@ -3,7 +3,25 @@ function curl(string $source){
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $source);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-    $data = curl_exec($source);
+    $data = curl_exec($ch);
     curl_close($ch);
-    return $data; 
+    return $data;
 }
+$subject = "https://www.otomoto.pl/oferta/bmw-seria-4-leasing-do-przejecia-cesja-zgoda-na-ubezpieczenie-obce-ID6zoTxu.html";
+$data = curl($subject);
+function pregMatchName(string $data){
+    $pattern = "#big-text\">\s+[A-Z]+#";
+    preg_match($pattern, $data, $matches);
+    //return $matches;
+    $name = $matches[0];
+    $patternToReplace = "#[^A-Z]+#";
+    $replaced = preg_replace($patternToReplace, "", $name);
+    return "Name: ".$replaced;
+
+}
+echo pregMatchName($data);
+
+
+
+
+
