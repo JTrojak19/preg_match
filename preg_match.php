5c3edf752b9ceaa4a5a78fe4cde305af3242c848
Joanna Trojak <joannatrojak19@gmail.com>
2017-12-18 09:38:08 +0100

    curl

--- preg_match.php
+++ preg_match.php
@@ -1,4 +1,9 @@
 <?php
 function curl(string $source){
-    
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $source);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+    $data = curl_exec($source);
+    curl_close($ch);
+    return $data; 
 }
